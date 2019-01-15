5e4e3bf84a5c8a78c6810f08f44d0a12f23f6270
daidai251 <1349322027@qq.com>
2019-01-16 00:18:35 +0800

    0116提交编辑器
    填了playerTools和tools，添加快速按钮

---
+++ UnityProject/Assets/Editor/PlayTools.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlayTools{
+    [MenuItem("CONTEXT/PlayerHealth/InitHeathAndSp")]
+    static void InitHeathAndSpeed(MenuCommand cmd) {
+        Debug.Log(cmd.context.GetType().FullName);
+      CompleteProject.PlayerHealth health = cmd.context as CompleteProject.PlayerHealth;
+        health.startingHealth = 200;
+        health.flashSpeed = 10;
+
+        Debug.Log("Init");
+    }
+
+    [MenuItem("CONTEXT/Rigidbody/Clean")]
+    static void CleanMassAndGravity(MenuCommand cmd) {
+        Rigidbody rgb = cmd.context as Rigidbody;
+        rgb.mass = 0;
+        rgb.useGravity = false;
+
+    }
+
+
+}
---
+++ UnityProject/Assets/Editor/Tools.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Tools {
+    [MenuItem("Tools/test1",false ,1)]
+    static void Test() {
+        Debug.Log("Test");
+    }
+    [MenuItem("Tools/MyDelet",false,20)]
+    static void Test2()
+    {
+        //Debug.Log(Selection.transforms);
+        Debug.Log(Selection.activeGameObject.name);//选择第一个
+        Debug.Log(Selection.objects.Length);
+        Debug.Log("Test");
+
+        foreach (Object ob in Selection.objects) {
+            //GameObject.Destroy(ob); //编辑模式下用
+            //GameObject.DestroyImmediate(ob);
+
+            //Undo 把操作记录写入Unity中，然后能撤回
+            Undo.DestroyObjectImmediate(ob);
+        }
+
+
+    }
+
+
+    [MenuItem("GameObject/MyDelet2", false, 20)]
+    static void Test3()
+    {
+        //Debug.Log(Selection.transforms);
+        Debug.Log(Selection.activeGameObject.name);//选择第一个
+        Debug.Log(Selection.objects.Length);
+        Debug.Log("Test");
+
+        foreach (Object ob in Selection.objects)
+        {
+            //GameObject.Destroy(ob); //编辑模式下用
+            GameObject.DestroyImmediate(ob);
+        }
+
+
+    }
+
+
+}
